91bfc8252fc7df2e03b0f2bf11da3b622caa63e3
SC Sohel <scmsohel@gmail.com>
2026-08-17 17:48:56 +0600

    Trigger final Walton bundle sync

--- remote-designs/walton-ceiling-fan.js
+++ remote-designs/walton-ceiling-fan.js
@@ -1,4 +1,4 @@
-// Modern segmented Walton remote layout v2.
+// Modern segmented Walton remote layout v3.
 export const waltonCeilingFanRemote = {
   id: 'walton-ceiling-fan',
   label: 'Walton Ceiling Fan',
