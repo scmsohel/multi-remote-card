bc1a9e6305ee5cf1c35b0e62bf82c3397ba04ff2
SC Sohel <scmsohel@gmail.com>
2026-08-17 17:49:37 +0600

    Trigger robust Walton bundle

--- remote-designs/walton-ceiling-fan.js
+++ remote-designs/walton-ceiling-fan.js
@@ -1,4 +1,4 @@
-// Modern segmented Walton remote layout v3.
+// Modern segmented Walton remote layout v4.
 export const waltonCeilingFanRemote = {
   id: 'walton-ceiling-fan',
   label: 'Walton Ceiling Fan',
